cb051ae86d7d35879b5827fbfb2d8aad058d3205
Muhammad Arslan <mughalarslan996@gmail.com>
2022-01-06 11:17:29 -0800

    modal changing styling account page redirect
    modal changing and styling of modal, and account page redirect handling and login form show hide handling on login

--- account.php
+++ account.php
@@ -1,9 +1,7 @@
 <?php
 include("header.php");
 include("dbcon.php");
-if (isset($_SESSION['user_id'])) {
-    header("location: index.php ");
-}
+
 ?>
 
 <!DOCTYPE html>
@@ -15,138 +13,140 @@ if (isset($_SESSION['user_id'])) {
 
 <body>
     <?php
-    include("totop.php");
+        include("totop.php");
+        $style = '';
+        // <!-- Register User code -->
+        if (isset($_POST['register_btn'])) {
+            $user_name = mysqli_real_escape_string($con, $_POST['txt_name']);
+            $user_email = mysqli_real_escape_string($con, $_POST['txt_email']);
+            $user_phone = mysqli_real_escape_string($con, $_POST['txt_phone']);
+            $user_password = mysqli_real_escape_string($con, $_POST['txt_password']);
+            $user_compassword = mysqli_real_escape_string($con, $_POST['txt_confirm_password']);
+            $date = date("Y-m-d h:i:sa");
 
-    // <!-- Register User code -->
-    if (isset($_POST['register_btn'])) {
-        $user_name = mysqli_real_escape_string($con, $_POST['txt_name']);
-        $user_email = mysqli_real_escape_string($con, $_POST['txt_email']);
-        $user_phone = mysqli_real_escape_string($con, $_POST['txt_phone']);
-        $user_password = mysqli_real_escape_string($con, $_POST['txt_password']);
-        $user_compassword = mysqli_real_escape_string($con, $_POST['txt_confirm_password']);
-        $date = date("Y-m-d h:i:sa");
 
+            $pass = password_hash($user_password, PASSWORD_BCRYPT);
+            $cpass = password_hash($user_compassword, PASSWORD_BCRYPT);
+            $user_role = 'U';
 
-        $pass = password_hash($user_password, PASSWORD_BCRYPT);
-        $cpass = password_hash($user_compassword, PASSWORD_BCRYPT);
-        $user_role = 'U';
+            $emailquery = "SELECT * FROM `user` WHERE `user_email` = '$user_email'";
+            $emailresult = mysqli_query($con, $emailquery);
+            $emailcount = mysqli_num_rows($emailresult);
 
-        $emailquery = "SELECT * FROM `user` WHERE `user_email` = '$user_email'";
-        $emailresult = mysqli_query($con, $emailquery);
-        $emailcount = mysqli_num_rows($emailresult);
-
-        if ($emailcount > 0) {
-            echo '<script>
-                                        swal({
-                                            title: "Error",
-                                            text: "Email Already Exist!",
-                                            icon: "warning",
-                                        });
-                                        </script>';
-        } else {
-
-            if ($user_password === $user_compassword) {
-                $insertquery = "INSERT INTO `user`(`date`, `user_name`, `user_email`, `user_phone`, `user_password`, `user_confirm_password`, `user_role`) 
-                                        VALUES ('$date','$user_name','$user_email','$user_phone','$pass','$cpass','$user_role')";
-
-                $queryfire = mysqli_query($con, $insertquery);
+            if ($emailcount > 0) {
                 echo '<script>
-                                        swal({
-                                            title: "Great News!",
-                                            text: "Your Are Registered Successfully",
-                                            icon: "success",
-                                        }).then(function(){
-                                            window.reload();
-                                        });
-                                        </script>';
+                                            swal({
+                                                title: "Error",
+                                                text: "Email Already Exist!",
+                                                icon: "warning",
+                                            });
+                                            </script>';
             } else {
-                echo '<script>
-                                        swal({
-                                            title: "Error",
-                                            text: "Password! Does Not Matched Correctly!",
-                                            icon: "warning",
-                                        });
-                                        </script>';
-            }
-        }
-    }
 
+                if ($user_password === $user_compassword) {
+                    $insertquery = "INSERT INTO `user`(`date`, `user_name`, `user_email`, `user_phone`, `user_password`, `user_confirm_password`, `user_role`) 
+                                            VALUES ('$date','$user_name','$user_email','$user_phone','$pass','$cpass','$user_role')";
 
-    // Login User
-
-    if (isset($_POST['login_btn'])) {
-
-        $useremail = mysqli_real_escape_string($con, $_POST['useremail']);
-        $password = mysqli_real_escape_string($con, $_POST['password']);
-
-        $checkemail = "SELECT * FROM `user` WHERE `user_email` = '$useremail' ";
-        $emailcheck = mysqli_query($con, $checkemail);
-
-        $mailcount = mysqli_num_rows($emailcheck);
-
-        if ($mailcount > 0) {
-            $db_data = mysqli_fetch_assoc($emailcheck);
-
-            $db_pass = $db_data['user_password'];
-
-            $_SESSION['user_id'] = $db_data['user_id'];
-            // var_dump($_SESSION['user_id']);
-            // exit;
-            $_SESSION['user'] = $db_data['user_name'];
-
-            $_SESSION['email'] = $db_data['user_email'];
-
-            //First $password is that password which comes from input type
-            //and the second is which we get from the databse 
-            //Password verify is to check the necrypted password and the password in database in hashing format
-
-            $pass_decode = password_verify($password, $db_pass);
-            if ($pass_decode) {
-
-                if (isset($_POST['rememberme'])) {
-                    setcookie('EmailCookie', $useremail, time() + (86400 * 30));
-                    setcookie('PasswordCookie', $password, time() + (86400 * 30));
+                    $queryfire = mysqli_query($con, $insertquery);
+                    echo '<script>
+                                            swal({
+                                                title: "Great News!",
+                                                text: "Your Are Registered Successfully",
+                                                icon: "success",
+                                            }).then(function(){
+                                                window.reload();
+                                            });
+                                            </script>';
+                } else {
+                    echo '<script>
+                                            swal({
+                                                title: "Error",
+                                                text: "Password! Does Not Matched Correctly!",
+                                                icon: "warning",
+                                            });
+                                            </script>';
                 }
-                // alert("Congratulation! you are logged in successfully");
-                header('location: index.php');
+            }
+        }
+        // Login User
+
+        if (isset($_POST['login_btn'])) {
+
+            $useremail = mysqli_real_escape_string($con, $_POST['useremail']);
+            $password = mysqli_real_escape_string($con, $_POST['password']);
+
+            $checkemail = "SELECT * FROM `user` WHERE `user_email` = '$useremail' ";
+            $emailcheck = mysqli_query($con, $checkemail);
+
+            $mailcount = mysqli_num_rows($emailcheck);
+
+            if ($mailcount > 0) {
+                $db_data = mysqli_fetch_assoc($emailcheck);
+
+                $db_pass = $db_data['user_password'];
+
+                $_SESSION['user_id'] = $db_data['user_id'];
+                $_SESSION['user'] = $db_data['user_name'];
+                $_SESSION['email'] = $db_data['user_email'];
+
+                //First $password is that password which comes from input type
+                //and the second is which we get from the databse 
+                //Password verify is to check the necrypted password and the password in database in hashing format
+
+                $pass_decode = password_verify($password, $db_pass);
+                if ($pass_decode) {
+
+                    if (isset($_POST['rememberme'])) {
+                        setcookie('EmailCookie', $useremail, time() + (86400 * 30));
+                        setcookie('PasswordCookie', $password, time() + (86400 * 30));
+                    }
+                    // alert("Congratulation! you are logged in successfully");
+                    // header('location: index.php');
+                    ?>
+                        <script>
+                            window.location.href = window.location.href;
+                        </script>
+                    <?php
+                } else {
+                    echo '<script>
+                        swal({
+                            title: "Error Occured",
+                            text: "Password! Does Not Matched",
+                            icon: "warning",
+                        });
+                        </script>';
+                }
             } else {
                 echo '<script>
-                    swal({
-                        title: "Error Occured",
-                        text: "Password! Does Not Matched",
-                        icon: "warning",
-                    });
-                    </script>';
+                        swal({
+                            title: "Error Occured",
+                            text: "Invalid Email or password ",
+                            icon: "warning",
+                        });
+                        </script>';
             }
-        } else {
-            echo '<script>
-                    swal({
-                        title: "Error Occured",
-                        text: "Invalid Email or password ",
-                        icon: "warning",
-                    });
-                    </script>';
         }
-    }
     ?>
     <!-- Account Page -->
-    <div class="account-page">
+    <?php 
+        if(isset($_SESSION['email']) && $_SESSION['email'])
+        {
+            $style = 'display: none'; 
+        }
+    ?>
+    <div class="account-page" >
         <div class="container">
             <div class="row">
                 <div class="col-2">
                     <img src="img/image1.png" alt="image" width="100%">
                 </div>
-                <div class="col-2">
-                    <div class="form-container">
+                <div class="col-2" style="<?php echo $style?>">
+                    <div class="form-container" >
                         <div class="form-btn">
                             <span onclick="login()">Login</span>
                             <span onclick="register()">Register</span>
                             <hr id="indicator">
                         </div>
-
-                        <?php
-
-                        ?>
 
                         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" id="LoginForm" method="POST">
                             <input type="email" placeholder="email" name="useremail" value="<?php if (isset($_COOKIE['EmailCookie'])) {
