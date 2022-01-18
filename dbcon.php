7d799a2badf22088d80404297da9eb618234568a
Muhammad Arslan <mughalarslan996@gmail.com>
2022-01-18 16:52:41 +0500

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -9,9 +9,9 @@ $dbname = "store";
 $con = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
-// if($con){
-//     echo "Connection Established Successfully";
-// }else{
-//     echo "Error Occured";
-// }
+ if($con){
+     echo "Connection Established Successfully";
+ }else{
+     echo "Error Occured";
+ }
 ?>
